88ca04ddeb3e82c32161b1710bee468c08da49fb
venkateshrajan <venkatesh.xaviers@gmail.com>
2025-02-16 21:00:54 +0530

    added kill switch and cpu fix

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,9 +1,9 @@
-
-use sysinfo::System;
+use log::{error, info};
+use sysinfo::{CpuRefreshKind, Pid, ProcessRefreshKind, RefreshKind, System};
 
 #[derive(Debug, Clone, serde::Serialize)]
 pub struct Process {
-    pid : u32,
+    pid: u32,
     name: String,
     cpu: f32,
     mem: u64,
@@ -11,24 +11,61 @@ pub struct Process {
 
 #[tauri::command]
 fn get_processes() -> Vec<Process> {
-    let mut system = System::new_all();
-    system.refresh_all();
+    let mut system = System::new_with_specifics(
+        RefreshKind::nothing()
+            .with_cpu(CpuRefreshKind::everything())
+            .with_processes(ProcessRefreshKind::nothing().with_cpu().with_memory()),
+    );
+    std::thread::sleep(sysinfo::MINIMUM_CPU_UPDATE_INTERVAL);
+    system.refresh_specifics(
+        RefreshKind::nothing()
+            .with_cpu(CpuRefreshKind::everything())
+            .with_processes(ProcessRefreshKind::nothing().with_cpu().with_memory()),
+    );
 
-    system.processes().iter().map(|(&pid, process)| {
-        Process {
-            pid : pid.as_u32(),
+    let cpu_count = system
+        .physical_core_count()
+        .expect("Couldn't get the physical core count") as f32;
+    system
+        .processes()
+        .iter()
+        .map(|(&pid, process)| Process {
+            pid: pid.as_u32(),
             name: process.name().to_string_lossy().to_string(),
-            cpu: process.cpu_usage(),
+            cpu: process.cpu_usage() / cpu_count,
             mem: process.memory(),
+        })
+        .collect()
+}
+
+#[tauri::command]
+fn kill_process(pid: usize) {
+    let mut system = System::new_with_specifics(
+        RefreshKind::nothing().with_processes(ProcessRefreshKind::nothing()),
+    );
+    system.refresh_specifics(RefreshKind::nothing().with_processes(ProcessRefreshKind::nothing()));
+
+    match system.process(Pid::from(pid)) {
+        Some(p) => {
+            if p.kill() {
+                info!("Pid {} killed successfully", pid);
+            } else {
+                error!("Failed to kill {}", pid);
+            }
         }
-    }).collect()
+        None => {
+            info!("Pid {} not found", pid);
+        }
+    }
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    env_logger::init();
+
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![get_processes])
+        .invoke_handler(tauri::generate_handler![get_processes, kill_process])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
@@ -41,11 +78,20 @@ mod tests {
     fn test_get_processes() {
         let processes = get_processes();
         assert!(!processes.is_empty(), "Process list should not be empty");
-        assert!(processes.iter().all(|p| !p.name.is_empty()), "All Processes should have a name");
+        assert!(
+            processes.iter().all(|p| !p.name.is_empty()),
+            "All Processes should have a name"
+        );
 
-        println!("{:<10} {:<30} {:<10} {:<10}", "PID", "Name", "CPU (%)", "Memory (KB)");
+        println!(
+            "{:<10} {:<30} {:<10} {:<10}",
+            "PID", "Name", "CPU (%)", "Memory (KB)"
+        );
         for process in processes {
-            println!("{:<10} {:<30} {:<10.2} {:<10}", process.pid, process.name, process.cpu, process.mem);
+            println!(
+                "{:<10} {:<30} {:<10.2} {:<10}",
+                process.pid, process.name, process.cpu, process.mem
+            );
         }
     }
 }
